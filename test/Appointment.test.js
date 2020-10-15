4681ee7940424e4f4bf900200938b119dd502f26
Jadir Junior <jadirjsjunior@gmail.com>
2020-10-15 19:12:48 -0300

    test of component appointment

---
+++ test/Appointment.test.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Appointment } from "../src/Appointment";
+
+describe("Appointment", () => {
+  let customer;
+  let container;
+
+  beforeEach(() => {
+    container = document.createElement("div");
+  });
+
+  const render = (component) => ReactDOM.render(component, container);
+
+  it("renders the customer first name", () => {
+    customer = { firstName: "Ashley" };
+    render(<Appointment customer={customer} />);
+    expect(container.textContent).toMatch("Ashley");
+  });
+
+  it("renders another customer first name", () => {
+    customer = { firstName: "Jordan" };
+    render(<Appointment customer={customer} />);
+    expect(container.textContent).toMatch("Jordan");
+  });
+});
