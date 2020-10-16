1a970b6cd5066d5698cb781d220e06778df3716e
Jadir Junior <jadirjsjunior@gmail.com>
2020-10-16 15:30:28 -0300

    extracting a form-finder method

--- test/CustomerForm.test.js
+++ test/CustomerForm.test.js
@@ -9,8 +9,10 @@ describe("CustomerForm", () => {
     ({ render, container } = createContainer());
   });
 
+  const form = (id) => container.querySelector(`form[id="${id}"]`);
+
   it("renders a form", () => {
     render(<CustomerForm />);
-    expect(container.querySelector('form[id="customer"]')).not.toBeNull();
+    expect(form("customer")).not.toBeNull();
   });
 });
