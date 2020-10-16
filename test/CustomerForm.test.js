561e4894ed73774c25d97f24f7b9220bdea2f5b7
Jadir Junior <jadirjsjunior@gmail.com>
2020-10-16 16:00:59 -0300

    extracting out a field-finder function

--- test/CustomerForm.test.js
+++ test/CustomerForm.test.js
@@ -11,6 +11,8 @@ describe("CustomerForm", () => {
 
   const form = (id) => container.querySelector(`form[id="${id}"]`);
 
+  const firstNameField = () => form("customer").elements.firstName;
+
   const expectToBeInputFieldOfTypeText = (formElement) => {
     expect(formElement).not.toBeNull();
     expect(formElement.tagName).toEqual("INPUT");
@@ -24,13 +26,11 @@ describe("CustomerForm", () => {
 
   it("renders the first name field as a text box", () => {
     render(<CustomerForm />);
-    const field = form("customer").elements.firstName;
-    expectToBeInputFieldOfTypeText(field);
+    expectToBeInputFieldOfTypeText(firstNameField());
   });
 
   it("includes the existing value for the first name", () => {
     render(<CustomerForm firstName="Ashley" />);
-    const field = form("customer").elements.firstName;
-    expect(field.value).toEqual("Ashley");
+    expect(firstNameField().value).toEqual("Ashley");
   });
 });
