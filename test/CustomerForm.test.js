ca40f9ae602d300444397e9fd8c3aae8553f23e1
Jadir Junior <jadirjsjunior@gmail.com>
2020-10-16 15:52:25 -0300

    extracting an expectation group function

--- test/CustomerForm.test.js
+++ test/CustomerForm.test.js
@@ -11,6 +11,12 @@ describe("CustomerForm", () => {
 
   const form = (id) => container.querySelector(`form[id="${id}"]`);
 
+  const expectToBeInputFieldOfTypeText = (formElement) => {
+    expect(formElement).not.toBeNull();
+    expect(formElement.tagName).toEqual("INPUT");
+    expect(formElement.type).toEqual("text");
+  };
+
   it("renders a form", () => {
     render(<CustomerForm />);
     expect(form("customer")).not.toBeNull();
@@ -19,8 +25,6 @@ describe("CustomerForm", () => {
   it("renders the first name field as a text box", () => {
     render(<CustomerForm />);
     const field = form("customer").elements.firstName;
-    expect(field).not.toBeNull();
-    expect(field.tagName).toEqual("INPUT");
-    expect(field.type).toEqual("text");
+    expectToBeInputFieldOfTypeText(field);
   });
 });
