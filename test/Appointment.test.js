b3f621d468f74cbcd19fb31f139ae24095525f8a
Jadir Junior <jadirjsjunior@gmail.com>
2020-10-16 11:31:04 -0300

    selecting data to view

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ReactTestUtils from "react-dom/test-utils";
 import { Appointment, AppointmentsDayView } from "../src/Appointment";
 
 describe("Appointment", () => {
@@ -29,8 +30,8 @@ describe("AppoinmentsDayView", () => {
   let container;
   const today = new Date();
   const appointments = [
-    { startsAt: today.setHours(12, 0) },
-    { startsAt: today.setHours(13, 0) },
+    { startsAt: today.setHours(12, 0), customer: { firstName: "Ashley" } },
+    { startsAt: today.setHours(13, 0), customer: { firstName: "Jordan" } },
   ];
 
   beforeEach(() => {
@@ -55,5 +56,30 @@ describe("AppoinmentsDayView", () => {
     expect(container.querySelectorAll("li")).toHaveLength(2);
     expect(container.querySelectorAll("li")[0].textContent).toEqual("12:00");
     expect(container.querySelectorAll("li")[1].textContent).toEqual("13:00");
+  });
+
+  it("initially shows a message saying there are no appointments today", () => {
+    render(<AppointmentsDayView appointments={[]} />);
+    expect(container.textContent).toMatch(
+      "There are no appointments scheduled for today."
+    );
+  });
+
+  it("selects the first appointment by default", () => {
+    render(<AppointmentsDayView appointments={appointments} />);
+    expect(container.textContent).toMatch("Ashley");
+  });
+
+  it("has a button element in each li", () => {
+    render(<AppointmentsDayView appointments={appointments} />);
+    expect(container.querySelectorAll("li > button")).toHaveLength(2);
+    expect(container.querySelectorAll("li > button")[0].type).toEqual("button");
+  });
+
+  it("renders another appointment when selected", () => {
+    render(<AppointmentsDayView appointments={appointments} />);
+    const button = container.querySelectorAll("button")[1];
+    ReactTestUtils.Simulate.click(button);
+    expect(container.textContent).toMatch("Jordan");
   });
 });
