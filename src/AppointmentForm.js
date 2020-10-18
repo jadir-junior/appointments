f60acba3ce88f9a2c28caec0f92a81b6951dd312
Jadir Junior <jadirjsjunior@gmail.com>
2020-10-17 21:41:36 -0300

    Providing options to a dropdown

---
+++ src/AppointmentForm.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export const AppointmentForm = ({ selectableServices }) => (
+  <form id="appointment">
+    <select name="service">
+      <option></option>
+      {selectableServices.map((s) => (
+        <option key={s}>{s}</option>
+      ))}
+    </select>
+  </form>
+);
+
+AppointmentForm.defaultProps = {
+  selectableServices: [
+    "Cut",
+    "Blow-dry",
+    "Cut & color",
+    "Beard trim",
+    "Cut & beard trim",
+    "Extensions",
+  ],
+};
